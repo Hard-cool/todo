3d25d1321bfbcc350aec82c61fac205cd6c72ff7
Hard-cool <artyomtitov99@mail.ru>
2021-09-15 17:47:27 +0300

    15.09.21

---
+++ todo/notes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from notes.forms import AddNoteForm
+
+
+def index(request):
+   notes = []
+   if request.method == "POST":
+       form = AddNoteForm(request.POST)
+       if form.is_valid():
+           # Create new DB record
+           pass
+   else:
+       form = AddNoteForm()
+   return render(request, "note_list.html", {"notes": notes, "form": form})
